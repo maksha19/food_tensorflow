5a48266f0fdae4e6e2ed169a15b0e7d388b896e8
maksha19 <maksha19@outlook.com>
2022-11-07 13:30:35 +0800

    disable logBoxs messages

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import {  LogBox } from "react-native";
+
+LogBox.ignoreAllLogs();
 
 function App() {
   const isLoadingComplete = useCachedResources();
